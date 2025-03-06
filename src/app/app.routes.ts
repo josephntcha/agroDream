ea098acee6c0efaa05a9444ffa2a3c5b9404571d
josephntcha <kouagoujosephn@gmail.com>
2025-03-06 23:01:44 +0100

    first commit for agroDream website

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { AccueilComponent } from './accueil/accueil.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    
+    {path:"", component:AccueilComponent}
+];
